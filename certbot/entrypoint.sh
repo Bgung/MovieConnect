31c83378d6f80755e27dd0783a4c4abe8ba46cfc
MinjaeKim <barrow13512@gmail.com>
2025-01-04 02:52:04 +0900

    idk

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # 인증서 발급 또는 갱신
-certbot certonly --webroot --webroot-path=/var/www/certbot --email ${EMAIL} --agree-tos --no-eff-email -d ${DOMAIN} --non-interactive
+# docker-compose.yaml env_file 지정
+# env_file에 들어갈 내용
+# EMAIL, DOMAIN
+certbot certonly -v --webroot --webroot-path=/var/www/html --email ${EMAIL} --agree-tos --no-eff-email -d ${DOMAIN} --non-interactive # --force-renewal
+
+# 권한 수정 안하면 Host에서 안보임
+chmod -R 777 /etc/letsencrypt/
 
 # cron 등록 (매분 갱신; 테스트용)
 # echo '* * * * * certbot renew --dry-run' > /etc/crontabs/root
@@ -10,4 +16,5 @@ certbot certonly --webroot --webroot-path=/var/www/certbot --email ${EMAIL} --ag
 echo '0 0 * * 0 certbot renew' > /etc/crontabs/root
 
 # crond 실행 # -f 옵션은 foreground로 실행
+# daemon으로 실행시 자동 갱신 불가
 exec crond -f
